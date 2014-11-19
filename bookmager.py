ff6e6fcbf523c0c4530c0e075b399bbed4c93653
thitigear <s7070031@kmitl.ac.th>
2014-11-19 21:52:43 +0700

    bookmagerV3
    เพิ่มDatabase (by Both)
    เพิ่มฟังก์ชัน add_book เพื่อใช้ในการแอดบุ๊คมาร์ค ลง database(by
    thitigear)
    เพิ่มฟังก์ชันในการอ่านบุ๊คมาร์ค โดยจะปริ้นที่ IDLE (by blackhen)

--- bookmager.py
+++ bookmager.py
@@ -1,10 +1,18 @@
 ''' Bookmager '''
 from Tkinter import *
 from webbrowser import *
-from sqlite3 import *
-
+import sqlite3
 root = Tk()
 menubar = Menu(root)
+var = StringVar()
+var.set("Please insert your URL")
+
+#sql
+con = sqlite3.connect('bookmark.db')
+cur = con.cursor()
+
+#create table (run at first time only but now isn't complete) if have tst.db on Desktop please go nextline to comment
+#cur.execute('create table bookmark (url text)')
 
 #Toplevel1
 def toplv():
@@ -12,9 +20,27 @@ def toplv():
 
 #OpenWEB
 def open_web():
-    web = 'https://www.google.co.th/?gws_rd=ssl#q=' + seach_entry.get()
-    open(web)
+    web = seach_entry.get()
+    if 'www.' in web:
+        open(web)
+    else:
+        web = 'https://www.google.co.th/?gws_rd=ssl#q=' + web
+        open(web)
+def add_book():
+    web = seach_entry.get()
+    if 'www.' in web:
+        cur.execute("insert into bookmark values ('%s')" % web)
+    else:
+        web = 'https://www.google.co.th/?gws_rd=ssl#q=' + web
+        cur.execute("insert into bookmark values ('%s')" % web)
+    con.commit()
+    
 
+def read_data():
+    for  row in cur.execute('SELECT * FROM bookmark'):
+        print row
+    print '=========================='
+    
 class Make_menu(Menu):
     def __init__(self):
         self.menu = Menu(menubar, tearoff=0)
@@ -27,25 +53,28 @@ class Make_menu(Menu):
         menubar.add_cascade(label="Home", menu=self.menu)
         root.config(menu=menubar)
 
-
 home = Make_menu().make_menu()
 
 #Seach
-var = StringVar()
-var.set("Please insert your URL")
     #Frame
 seach_frame = LabelFrame(root, text="Seach", padx=5, pady=5)
 seach_frame.pack(padx=10, pady=10)
+
     #Entry
 seach_entry = Entry(seach_frame, textvariable=var)
 seach_entry.pack()
     #Seach Button
 seach_but = Button(seach_frame, text = "Seach", command=open_web, bg="white",\
-              fg = "black")
+                      fg = "black")
 seach_but.pack(fill = "both", expand=1)
     #Add Bookmark Button
-add_b_but = Button(seach_frame, text = "Add Bookmark", command=None, bg="white",\
-              fg = "black")
-add_b_but.pack(fill = "both", expand=1)
+seach_book = Button(seach_frame, text = "Add Bookmark", command=add_book, bg="white",\
+                      fg = "black")
+seach_book.pack(fill = "both", expand=1)
+
+#Read Bookmark Button
+read_book = Button(seach_frame, text = "Read Bookmark", command=read_data, bg="white",\
+                      fg = "black")
+read_book.pack(fill = "both", expand=1)
 
 root.mainloop()
