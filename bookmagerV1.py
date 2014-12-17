e6aa3f1a96dc53254d2cb1547adeeb76b46a9eae
blackhen <s7070019@kmitl.ac.th>
2014-12-17 17:22:39 +0700

    Last Fix
    Last Fix

--- bookmagerV1.py
+++ bookmagerV1.py
@@ -2,7 +2,7 @@
 from Tkinter import *
 import webbrowser, sqlite3, os
 cwd = os.getcwd()
-con = sqlite3.connect('bookmark.db')
+con = sqlite3.connect('default.db')
 cur = con.cursor()
 ##
 ##                            #New Bookmark
@@ -14,56 +14,6 @@ def new_bookmark():
 def choose_bm():
     """ link bookmark to save bookmark in that file """
     choose_book = Choose_bookmark()
-##------------------------------------------------------------------------------------------------
-##                            #Open Web
-##def open_web():
-##    web = seach_entry.get()
-##    if 'www.' in web:
-##        webbrowser.open(web)
-##    else:
-##        web = 'https://www.google.co.th/?gws_rd=ssl#q=' + web
-##        webbrowser.open(web)
-##------------------------------------------------------------------------------------------------
-##                            #Add Book
-##def add_book():
-##    """ Push URL to database file """
-##    web = seach_entry.get()
-##    if 'www.' in web:
-##        cur.execute("insert into bookmark values ('%s')" % web)
-##    else:
-##        web = 'https://www.google.co.th/?gws_rd=ssl#q=' + web
-##        cur.execute("insert into bookmark values ('%s')" % web)
-##    con.commit()
-##------------------------------------------------------------------------------------------------    
-##                            #Read Data
-##def OnRigthMouseClick(event):
-##    """ This event will run if right mouse was clicked """
-##    items = listbox.curselection()[0] #print index of data in listbox
-##    x = Open_web(data[items][0])
-##    x.open_web()
-##------------------------------------------------------------------------------------------------
-##                            #HELP PAGE
-##def help_page():
-##    """ Help page """
-##    help_page = Window("Help")
-##    help_frame = Frame(help_page.windows, text="Help User")
-##    help_frame.pack()
-##    help_label = Label(help_page.windows, text='%s' % open('developer.txt', 'r'))
-##    help_page.make_win()
-##------------------------------------------------------------------------------------------------
-##class Open_web(object):
-##    """ """
-##    def __init__(self, url):
-##        self.u = url
-##    def open_web(self):
-##        web = self.u
-##        if 'www.' in web:
-##            webbrowser.open(web)
-##        else:
-##            web = 'https://www.google.co.th/?gws_rd=ssl#q='
-##        webbrowser.open(web)
-##------------------------------------------------------------------------------------------------
-
 
 ##----------------------------------------------------------------------------------------        
                             #Menubar
@@ -81,46 +31,17 @@ class Window(Tk):
         #MENUBAR
         menubar = Menu(self.windows)
         home_mn = Menu(menubar, tearoff=0)
-##        help_mn = Menu(menubar, tearoff=0)
-##            #HOME MENU
         home_mn.add_command(label="New", command=new_bookmark)
         home_mn.add_command(label="Open", command=choose_bm)
-##        home_mn.add_command(label="Open Bookmark", command=self.read_data)
-##        home_mn.add_command(label="Export")
-##        home_mn.add_separator()
         home_mn.add_command(label="Exit", command=self.windows.destroy)
         menubar.add_cascade(label="Home", menu=home_mn)
         self.windows.config(menu=menubar)
-            #HELP MENU
-##        help_mn.add_command(label="Help", command=help_page)
-##        menubar.add_cascade(label="Help", menu=help_mn)
         mainloop()
+##------------------------------------------------------------------------------------------------
 
-##    def read_data(self):
-##        """ Get data from database file to listbox """
-##        read_page = Window("Bookmark List")
-##        i = 0
-##        global data
-##        data = {}
-##        for row in cur.execute('SELECT * FROM bookmark'):
-##            data[i] = row
-##            i += 1
-##            print data
-##
-##        scrollbar = Scrollbar(read_page.windows)
-##        global listbox
-##        listbox = Listbox(read_page.windows,\
-##                          yscrollcommand=scrollbar.set,\
-##                          width=50, selectmode=EXTENDED)
-##        listbox.config(selectbackground='orange')
-##        for i in data:
-##            listbox.insert(END, data[i])
-##        listbox.pack(side=LEFT, fill=BOTH)
-##        scrollbar.pack(sid=LEFT, fill=Y)
-##        scrollbar.config(command=listbox.yview)
-##        listbox.bind("<Double-Button-1>", OnRigthMouseClick)
-##re_data
+#Re Data
 def read_data_re():
+        '''refreash_data'''
         listbox.delete(0, END)
         i = 0
         global data
@@ -132,11 +53,13 @@ def read_data_re():
         for i in data:
             listbox.insert(END, data[i])
 ##------------------------------------------------------------------------------------------------
-    ##choose
+
+    
+##choose
 class Choose_bookmark(Window):
-    """ """
+    """Open Database"""
     def __init__(self):
-        """ """
+        """Main"""
         self.cb = Window("Open")
         #listbox
         num = 0
@@ -157,12 +80,34 @@ class Choose_bookmark(Window):
         self.scrollbar.pack(sid=LEFT, fill=Y)
         self.scrollbar.config(command=self.listbox.yview)
         self.listbox.bind("<Double-Button-1>", self.double_click)
-        self.change_but = Button(self.cb.windows, text="Open", command=self.choose_bookmark)
+        self.change_but = Button(self.cb.windows, text="Open", command=self.choose_bookmark, bg='#FFBF00')
         self.change_but.pack(fill=X)
-        self.cancel_but = Button(self.cb.windows, text="Cancel", command=self.choose_cancel)
+        self.cancel_but = Button(self.cb.windows, text="Cancel", command=self.choose_cancel, bg='#FFBF00')
         self.cancel_but.pack(fill=X)
+        self.delete_but = Button(self.cb.windows, text="Delete", command=self.choose_delete, bg='#FFBF00')
+        self.delete_but.pack(fill=X)
         self.cb.make_win()
         
+    def choose_delete(self):
+        '''Delete Database'''
+        self.items = int(self.listbox.curselection()[0]) #print index of data in listbox
+        print self.data[self.items]
+        os.remove(self.data[self.items])
+        self.choose_re()
+    
+    def choose_re(self):
+        '''Refreash Listbox'''
+        self.listbox.delete(0, END)
+        i = 0
+        num = 0
+        self.data = {}
+        for file_n in os.listdir(cwd):
+            if file_n.endswith('.db'):
+                self.data[num] = file_n
+                num += 1
+        print self.data
+        for num in self.data:
+            self.listbox.insert(END, self.data[num])
         
     def choose_bookmark(self):
         """ open wanted bookmark """
@@ -186,7 +131,7 @@ class Choose_bookmark(Window):
         self.cb.windows.destroy()
         
     def double_click(self, event):
-        """ """
+        """DoubleClick to open database"""
         self.items = int(self.listbox.curselection()[0]) #print index of data in listbox
         print self.data[self.items], cwd, "'"+self.data[self.items]+"'"
         global con
@@ -207,8 +152,11 @@ class Choose_bookmark(Window):
         
         
     def choose_cancel(self):
+        '''Exit OPen'''
         self.cb.windows.destroy()
+        
 ##------------------------------------------------------------------------------------------------
+##Main
 class Main_window(Window):
     """ Main programme window """
     def __init__(self, title):
@@ -219,39 +167,71 @@ class Main_window(Window):
         self.seach_frame.pack(side=LEFT, padx=10, pady=10)
         #url ENTRY
         urldefault = StringVar()
+        namedefault = StringVar()
+        keyworddefault = StringVar()
         self.seach_entry = Entry(self.seach_frame, textvariable=urldefault)
         self.seach_entry.pack()
         urldefault.set("URL")
         #name ENTRY
-        namedefault = StringVar()
         self.name_entry = Entry(self.seach_frame, textvariable=namedefault)
         self.name_entry.pack()
         namedefault.set("NAME")
-        #SEACH BUTTON
-        self.seach_but = Button(self.seach_frame, text = "Search",\
-                                command=self.op_web,\
-                                  fg = "black", bg='#FFBF00')
-        self.seach_but.pack(fill = "both", expand=1)
+
         #Add Bookmark Button
         self.seach_book = Button(self.seach_frame,\
                                  text = "Add Bookmark", command=self.add_book,\
                                  fg = "black", bg='#FFBF00')
         self.seach_book.pack(fill = "both", expand=1)
-        #Read Bookmark Button
-##        self.read_book = Button(self.seach_frame,\
-##                                text = "Read Bookmark", command=self.read_data,\
-##                                  fg = "black", bg='#FFBF00')
-##        self.read_book.pack(fill = "both", expand=1)
-        #list bookmark
+        #Delete
+        self.delete = Button(self.seach_frame,\
+                                 text = "Delete", command=self.delete,\
+                                 fg = "black", bg='#FFBF00')
+        self.delete.pack(fill = "both", expand=1)
 
+        #SEARCH FRAME
+        self.search_frame = LabelFrame(self.w.windows, bg='White', text="Search", padx=5, pady=5)
+        self.search_frame.pack(side=RIGHT, padx=10, pady=10)
         
-            
+        #SEARCH BUTTON
+        self.search_entry = Entry(self.search_frame, textvariable=keyworddefault)
+        self.search_entry.pack()
+        keyworddefault.set("Keyword")        
+        self.search_but = Button(self.search_frame, text = "Search Bookmark",\
+                                command=self.search_book,\
+                                  fg = "black", bg='#FFBF00')
+        self.search_but.pack(fill = "both", expand=1)
+
+        #OPEN Button
+        self.open_but = Button(self.search_frame, text = "OPEN URL",\
+                                command=self.op_web,\
+                                  fg = "black", bg='#FFBF00')
+        self.open_but.pack(fill=X, expand=1, side='left')
         
         self.read_data()
         self.w.make_win()
+
+    #search bookmark
+    def search_book(self):
+        '''Search bookmark'''
+        keyword = self.search_entry.get()
+        print keyword
+        listbox.delete(0, END)
+        i = 0
+        global data
+        data = {}
+        for row in cur.execute('SELECT * FROM bookmark'):
+            print row
+            if keyword in (row[0] or row[1]):
+                print 'yes'
+                data[i] = row[1]+' '*(15-len(row[1]))+row[0]
+                i += 1
+                print data  
+        for i in data:
+            listbox.insert(END, data[i])
         
-        
+    #list bookmark
     def read_data(self):
+        '''ListBox'''
         scrollbar = Scrollbar(self.w.windows)
         global listbox
         listbox = Listbox(self.w.windows,\
@@ -272,8 +252,22 @@ class Main_window(Window):
         listbox.pack(side=LEFT)
         scrollbar.pack(sid=RIGHT, fill=Y)
         scrollbar.config(command=listbox.yview)
-            
+        listbox.bind("<Double-Button-1>", self.double_open_url)
+
+    #Delete URL
+    def delete(self):
+        '''Delete bookmark'''
+        global cur, con
+        items = int(listbox.curselection()[0]) #print index of data in listbox
+        name_data = data[items].split()[0]
+        url_data = data[items].split()[1]
+        cur.execute("DELETE FROM bookmark WHERE name = '%s'" %name_data)
+        read_data_re()
+        con.commit()
+
+    #Re Data   
     def read_data_re(self):
+        '''Refreash Data'''
         listbox.delete(0, END)
         i = 0
         global data
@@ -284,16 +278,16 @@ class Main_window(Window):
             print data  
         for i in data:
             listbox.insert(END, data[i])
-            
+
+    #Open URL       
     def op_web(self):
         """ Open website """
-        web = self.seach_entry.get()
-        if 'www.' in web:
-            webbrowser.open(web)
-        else:
-            web = 'https://www.google.co.th/?gws_rd=ssl#q=' + web
-            webbrowser.open(web)
-            
+        self.items = int(listbox.curselection()[0]) #print index of data in listbox
+        print self.items
+        print data[self.items].split()[1]
+        webbrowser.open(data[self.items].split()[1])
+
+    #Add Bookmark       
     def add_book(self):
         """ Push URL to database file """
         web = self.seach_entry.get()
@@ -301,47 +295,32 @@ class Main_window(Window):
         cur.execute("insert into bookmark values ('"+web+"', '"+name+"')")
         read_data_re()
         con.commit()
+
+    #doubleclick to open
+    def double_open_url(self, event):
+        '''doubleclick to open'''
+        self.items = int(listbox.curselection()[0]) #print index of data in listbox
+        print data[self.items].split()[1]
+        webbrowser.open(data[self.items].split()[1])
         
-        
-##------------------------------------------------------------------------------------------------        
-##value = 1
-##class Check_first(Window):
-##    """ if first open, it will make a database's table """
-##    def __init__(self):
-##        """ Check First's structure """
-##        self.check = Window("Make Database Table")
-##        self.label1 = Label(text="First Run?", bg='Orange')
-##        self.label1.pack(fill=X)
-##        self.yes = Button(self.check.windows, text="Yes",\
-##                          command=self.choose_yes, bg='#FFBF00')
-##        self.yes.pack(fill=X)
-##        self.no = Button(self.check.windows, text="No",\
-##                         command=self.choose_no, bg='#FFBF00')
-##        self.no.pack(fill=X)
-##        mainloop()
-##    def choose_yes(self):
-##        """ Create database table before open the main window """
-##        self.check.windows.destroy()
-##        self.fisrt_creat =  New_bookmark_page()
-##        
-##    def choose_no(self):
-##        """ open the main window """
-##        self.check.windows.destroy()
 ##------------------------------------------------------------------------------------------------
+        
 class New_bookmark_page(Window):
         def __init__(self):
+            '''main'''
             self.first_bk = Window("Create Your Bookmark file")
             self.name_label = Label(self.first_bk.windows, text='please Insert your bookmark name')
             self.name_label.pack()
             self.name_entry = Entry(self.first_bk.windows)
             self.name_entry.pack()
-            self.creat_but = Button(self.first_bk.windows, text="Create File", command=self.new_bookmark)
+            self.creat_but = Button(self.first_bk.windows, text="Create File", command=self.new_bookmark, bg='#FFBF00')
             self.creat_but.pack(fill=X)
-            self.cancel_but = Button(self.first_bk.windows, text="Cancel", command=self.choose_cancel)
+            self.cancel_but = Button(self.first_bk.windows, text="Cancel", command=self.choose_cancel, bg='#FFBF00')
             self.cancel_but.pack(fill=X)
             self.first_bk.make_win()
+            
         def new_bookmark(self):
-            """ """
+            """Button Add"""
             self.bookmark_name = self.name_entry.get()
             global con
             global cur
@@ -352,12 +331,13 @@ class New_bookmark_page(Window):
             self.first_bk.windows.destroy()
             
         def choose_cancel(self):
+            '''Button Exit'''
             self.first_bk.windows.destroy()
+            
+
 ##------------------------------------------------------------------------------------------------
 
-
-##------------------------------------------------------------------------------------------------
-
+#Build First File database
 ##con = sqlite3.connect('test.db')
 ##con.execute('''CREATE TABLE bookmark
 ##             (url text, name text)''')
